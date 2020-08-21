336a957e53d1106331b053f4eb1945e778d59fff
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-08-21 09:09:25 +0530

    added middleware support to validator

--- validators/sample_validator.js
+++ validators/sample_validator.js
@@ -12,6 +12,28 @@ class SampleValidator extends Validator {
     super(data, rules, messages);
   }
 
+  /**
+   * 
+   * Static function that validates a incoming request as a middleware
+   * 
+   * @param {express.Request} req 
+   * @param {express.Response} res 
+   * @param {express.Next} next 
+   */
+  static middleware(req, res, next) {
+    const validator = new LoginValidator(req.body);
+    // check if data is valid
+    if (validator.fails()) {
+      // return error in case data invalid
+      return res.json(validator.errors());
+    } else {
+      // set validated values a req.body
+      req.body = validator.validated;
+      // continue with next middleware
+      next();
+    }
+  }
+
 }
 
 module.exports = SampleValidator;
