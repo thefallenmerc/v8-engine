c00124d9ebb47d619bf81dfac390e9a2d1cedf6d
Shubham Singh Chahar <thefallenmerc@gmail.com>
2020-08-21 08:24:09 +0530

    redid the validator

--- validators/sample_validator.js
+++ validators/sample_validator.js
@@ -1,79 +1,17 @@
-"use strict";
+const Validator = require('./validator');
 
-const ValidatorJS = require("validatorjs");
+class SampleValidator extends Validator {
 
-/**
- * Create Company Validator
- */
-class Validator {
-  /**
-   * Constructor
-   * @param {*} req 
-   */
-  constructor(req) {
+  constructor(data = {}, rules = {}, messages = {}) {
     /**
-     * Rules
+     * Define You rules here
      */
-    this.rules = {};
+    rules = {
+    };
 
-    /**
-     * Error Messages
-     */
-    this.messages = {};
-
-    /**
-     * Validated fields' object
-     */
-    this.validated = {};
-
-    /**
-     * Object to be validated
-     */
-    this.data = req.body;
-
-    /**
-     * Validator
-     */
-    this.validator = new ValidatorJS(this.data, this.rules, this.messages);
-
-    /**
-     * Validate all the fields
-     */
-    this.getValidated();
+    super(data, rules, messages);
   }
 
-  /**
-   * Check if the validator fails
-   */
-  fails() {
-    return this.validator.fails();
-  }
-
-  /**
-   * Check if the validator passes
-   */
-  passes() {
-    return this.validator.passes();
-  }
-
-  errors() {
-    return this.validator.errors;
-  }
-
-  /**
-   * Get only the validated content
-   */
-  getValidated(getNull = false) {
-    for (const rule in this.rules) {
-      if(this.data[rule] == undefined) {
-        if(getNull) {
-          this.validated[rule] = null;
-        }
-      } else {
-        this.validated[rule] = this.data[rule];
-      }
-    }
-  }
 }
 
-module.exports = Validator;
+module.exports = SampleValidator;
